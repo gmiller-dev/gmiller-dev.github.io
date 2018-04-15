5b93af0d2a980319d4a38814d60ee19c0f9389e3
Geoff Miller <geoffrey.miller.dev@gmail.com>
2018-04-15 17:41:22 -0500

    Make Navbar more reactive

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ const apply_filled_out = cntrl => has_value(cntrl)
                          ? cntrl.classList.add('filled-out')
                          : cntrl.classList.remove('filled-out')
 
-const formCntrls = Array.from(document.getElementsByClassName("form-control"));
+const formCntrls = Array.from(document.getElementsByClassName('form-control'));
 
 formCntrls.forEach( cntrl =>{
   apply_filled_out(cntrl)
   cntrl.addEventListener('focusout', () => apply_filled_out(cntrl))
 })
+
+const navbar_toggle = document.getElementById('navigation_toggle');
+const navigation = document.getElementById('navigation')
+
+
+navbar_toggle.onclick = () => navigation.classList.contains("navbar__items-collapse")
+  ? navigation.classList.remove('navbar__items-collapse')
+  : navigation.classList.add('navbar__items-collapse')
+
